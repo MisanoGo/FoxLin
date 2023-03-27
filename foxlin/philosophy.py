f712f58a6de15b67ae93ed419a40aaf4713ddd47
misano <misano.go@yahoo.com>
2023-03-27 19:21:48 +0000

    set db type & set crud classes op_name & define db carrier

--- foxlin/philosophy.py
+++ foxlin/philosophy.py
@@ -1,32 +1,43 @@
 from abc import ABC, abstractstaticmethod
+from typing import List, Dict, Union
 
 from pydantic import BaseModel
+
+VALID_TYPES = Union[str,int]
+DB_TYPE = Dict[str ,VALID_TYPES]
 
 class Schema(BaseModel):
     ID: int
 
+class DBCarrier(BaseModel):
+    db: DB_TYPE
 
 class DBOperation(BaseModel):
     op_name: str
     record : Schema
 
 class DBCreate(DBOperation):
-    pass
+    op_name: str = 'CREATE'
 
 class DBRead(DBOperation):
-    pass
+    op_name: str = "READ"
 
 class DBUpdate(DBOperation):
-    pass
+    op_name: str = "UPDATE"
 
 class DBDelete(DBOperation):
-    pass
+    op_name: str = "DELETE"
 
 class FileSystem(ABC):
+    def __init__(self, path: str):
+        raise NotImplementedError
+
     @abstractstaticmethod
-    def load(self):
-        pass
+    def load(self) -> DBCarrier:
+        raise NotImplementedError
 
     @abstractstaticmethod
     def operate(self, obj: DBOperation):
-        pass
+        raise NotImplementedError
+
+
