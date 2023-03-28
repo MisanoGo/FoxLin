f7ac7c5391db6e0262abbcf637700866f23f8bb0
misano <misano.go@yahoo.com>
2023-03-28 01:41:38 +0000

    change name of file system to FoxBox

--- foxlin/box.py
+++ foxlin/box.py
@@ -1,4 +1,37 @@
-from philosophy import FileSystem
+import pathlib
+from typing import Callable
 
-class FoxBox(FileSystem):
-    pass
+import orjson
+
+from philosophy import FoxBox, DBCarrier, DBOperation, DB_TYPE
+
+
+class CreateJsonDB(DBOperation):
+    path: str
+    op_name: str = "createDatabase"
+    base_schema: DBCarrier = DBCarrier(db={"ID":{0:0}})
+
+class JsonBox(FoxBox):
+    file_type = '.json'
+
+    def __init__(self, path: str):
+        self.path = pathlib.Path(path)
+
+        if path.endswith(self.file_type):
+            if self.path.exists():
+                with open(path,'r') as file:
+                    self.data = orjson.loads(file)
+
+            else:
+                self.operate(CreateJsonDB(path=path))
+
+    def load(self) -> DBCarrier:
+        return DBCarrier(db=self.data)
+
+    def operate(self,obj: DBOperation):
+        operator: Callable = self.__getattribute__(obj.op_name)
+        return operator(obj)
+
+    def createDatabase(self,obj: CreateJsonDB):
+        with open(obj.path,'w+',encoding='utf-8') as dbfile:
+            dbfile.write(obj.base_schema.json())
