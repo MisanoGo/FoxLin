c85a6542b1a35b3b86d0b5b8f0bcdcf820cb4863
misano <misano.go@yahoo.com>
2023-04-17 19:18:45 +0000

    set type hints

--- foxlin/utils.py
+++ foxlin/utils.py
@@ -1,15 +1,17 @@
+from typing import Dict, List, Any
+
 from .philosophy import Schema
 
 def migrate(path, obj: Schema):
     # TODO in 1.1
     pass
 
-def get_attr(obj, name):
+def get_attr(obj, name) -> Any:
     return object.__getattribute__(obj, name)
 
-def getKeyList(obj: Schema):
+def getKeyList(obj: Schema) -> List[str]:
     return obj.construct().schema()['properties'].keys()
 
-def getStructher(obj: Schema, k=dict()):
+def getStructher(obj: Schema, k=dict()) -> Dict[str, Any]:
     structuer = {c: k for c in getKeyList(obj)}
     return structuer
