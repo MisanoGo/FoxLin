20f38723d2f92cd5495f9a575a594a726bb6a966
misano <misano.go@yahoo.com>
2023-04-17 15:00:16 +0000

    set get_attr def

--- foxlin/utils.py
+++ foxlin/utils.py
@@ -1,8 +1,11 @@
 from pydantic import BaseModel
 
+
+def get_attr(obj, name):
+    return object.__getattribute__(obj, name)
+
 def getKeyList(obj: BaseModel):
     return obj.construct().schema()['properties'].keys()
-
 
 def getStructher(obj: BaseModel, k=dict()):
     structuer = {c: k for c in getKeyList(obj)}
