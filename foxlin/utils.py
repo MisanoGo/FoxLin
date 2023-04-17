bb0508bdc4ee47144cbc38afcf57114e24aad898
misano <misano.go@yahoo.com>
2023-04-17 19:15:35 +0000

    TODO migrate

--- foxlin/utils.py
+++ foxlin/utils.py
@@ -1,12 +1,15 @@
-from pydantic import BaseModel
+from .philosophy import Schema
 
+def migrate(path, obj: Schema):
+    # TODO in 1.1
+    pass
 
 def get_attr(obj, name):
     return object.__getattribute__(obj, name)
 
-def getKeyList(obj: BaseModel):
+def getKeyList(obj: Schema):
     return obj.construct().schema()['properties'].keys()
 
-def getStructher(obj: BaseModel, k=dict()):
+def getStructher(obj: Schema, k=dict()):
     structuer = {c: k for c in getKeyList(obj)}
     return structuer
